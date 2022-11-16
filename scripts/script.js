4c3c4c3c651014893b206db2f223d0d40d781882
mehrdad <mehrdad.ordobadi@gmail.com>
2022-11-16 14:18:46 -0800

    queryselector and what not

--- scripts/script.js
+++ scripts/script.js
@@ -10,8 +10,41 @@ function greet(promptname,dayTime = 'day'){
 // user = prompt("Name?:")
 // timewhen = prompt("Time?")
 // greet(user)
-function circleArea(radius){
-    return Math.PI*radius**2
+// function circleArea(radius){
+//     return Math.PI*radius**2
+// }
+// rad = prompt("Enter radius:")
+// alert(`The area of a circle with radius ${rad} is ${circleArea(rad).toFixed(2)}.`)
+// document.querySelector('#radius').textContent = `Value ${rad} provided by the user.`;
+// document.querySelector('#result').textContent = `The area of a circle with radius ${rad} is ${circleArea(rad).toFixed(2)}.`
+const shoppinglistElement = document.querySelector('.shopping')
+let shopping_items = ['cheese', 'bread', 'green pepper'];
+
+function listMaking(myArr){
+    for (item of myArr){
+       let listItem = document.createElement('li');
+       listItem.textContent = item;
+        shoppinglistElement.appendChild(listItem);
+    }
 }
-rad = prompt("Enter radius:")
-alert(`The area of a circle with radius ${rad} is ${circleArea(rad).toFixed(2)}.`)
+listMaking(shopping_items);
+
+// shoppinglistElement.appendChild(listMaking(shopping_items))
+
+
+function changeListStyle(myList) {
+    shoppinglistElement.classList.replace('circleList','squareList')
+}
+
+changeListStyle()
+
+function makeGreen(){
+    const allListItems = document.querySelectorAll('.shopping li')
+    for (item of allListItems){
+        if (item.textContent.includes('green')){
+            item.classList.add('greenlist');
+        }
+
+    }
+}
+makeGreen()
